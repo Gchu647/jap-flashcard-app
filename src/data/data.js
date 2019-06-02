694ebbd06725ae177bc6100f2943a1e31b70eb5c
George <g7chu@Temps-MacBook-Pro.local>
2019-06-02 11:22:44 -1000

    added 12 vocabs

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,95 @@
 const JapData = [
   {
+    kanji: '行く',
+    def: 'go',
+    romaji: '[Iku]',
+    speech_part: 'VERB',
+    id:1
+  },
+  {
+    kanji: '見る',
+    def: 'See, look at',
+    romaji: '[Miru]',
+    speech_part: 'VERB',
+    id:2
+  },
+  {
+    kanji: '多い',
+    def: 'A lot of, many',
+    romaji: '[Ōi]',
+    speech_part: 'ADJ.',
+    id:3
+  },
+  {
+    kanji: '家',
+    def: 'home, household',
+    romaji: '[Ie]',
+    speech_part: 'NOUN',
+    id:4
+  },
+  {
+    kanji: 'これ',
+    def: 'this, this one',
+    romaji: '[Kore]',
+    speech_part: 'PRONOUN',
+    id:5
+  },
+  {
+    kanji: 'それ',
+    def: 'that, that one',
+    romaji: '[Sore]',
+    speech_part: 'PRONOUN',
+    id:6
+  },
+  {
+    kanji: '私',
+    def: 'I (general)',
+    romaji: '[Watashi]',
+    speech_part: 'PRONOUN',
+    id:7
+  },
+  {
+    kanji: '仕事',
+    def: 'Work, job',
+    romaji: '[Shigoto]',
+    speech_part: 'VERBAL NOUN',
+    id:8
+  },
+  {
+    kanji: 'いつ',
+    def: 'When',
+    romaji: '[Itsu]',
+    speech_part: 'INTEROGATIVE',
+    id:9
+  },
+  {
+    kanji: 'する',
+    def: 'Do, make',
+    romaji: '[Suru]',
+    speech_part: 'VERB',
+    id:10
+  },
+  {
+    kanji: '出る',
+    def: 'Go out, leave',
+    romaji: '[Deru]',
+    speech_part: 'VERB',
+    id:11
+  },
+  {
     kanji: '使う',
     def: 'Use, make use of',
     romaji: '[Tsukau]',
-    speech_part: 'VERB'
+    speech_part: 'VERB',
+    id:12
   },
+  // {
+  //   kanji: '',
+  //   def: '',
+  //   romaji: '',
+  //   speech_part: '',
+  //   id:0
+  // },
 ];
 
 export default JapData;
