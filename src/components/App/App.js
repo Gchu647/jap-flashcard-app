9a778066c1d8deba30ea37e96839f6fe79421d10
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-06-02 15:23:51 -1000

    display single element from db based on id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import JapData from '../../data/data';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="flashcard-top">
-        <div className="kanji-wrap">
-          {JapData.map(vocab => (
-            <div className="kanji">{vocab.kanji}</div>
-          ))}
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      vocab: {}
+    }
+  }
+
+  componentDidMount() {
+    let vocabObj = JapData.find(vocab => {
+      return vocab.id === 10;
+    });
+
+    this.setState({ vocab: vocabObj });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="flashcard-top">
+          <div className="kanji-wrap">
+            <div className="kanji">
+              {this.state.vocab.kanji}
+            </div>
+          </div>
+        </div>
+        <div className="flashcard-btm">
+        <div className="def-wrap">
+            <div className="def">
+              {this.state.vocab.def}
+            </div>
+          </div>
+          <div className="romaji-wrap">
+            <div className="romaji">
+              {this.state.vocab.romaji}
+            </div>
+          </div>
+          <div className="speech-part-wrap">
+            <div className="speech-part">
+              {this.state.vocab.speech_part}
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flashcard-btm">
-      <div className="def-wrap">
-          <div className="def"></div>
-        </div>
-        <div className="romaji-wrap">
-          <div className="romaji"></div>
-        </div>
-        <div className="speech-part-wrap">
-          <div className="speech-part"></div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
