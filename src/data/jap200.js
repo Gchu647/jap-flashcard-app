e84f99603d5332302e946e857fe6d396189eaa1e
George <g7chu@Temps-MacBook-Pro.local>
2019-06-28 11:01:52 -1000

    now include 110 vocabs

--- src/data/jap200.js
+++ src/data/jap200.js
@@ -20,6 +20,55 @@ const JapData = [
     speech_part: 'VERB',
     id:103
   },
+  {
+    kanji: '見せる',
+    def: 'show',
+    romaji: '[miseru]',
+    speech_part: 'VERB',
+    id:104
+  },
+  {
+    kanji: '娘',
+    def: 'show',
+    romaji: '[musume]',
+    speech_part: 'NOUN',
+    id:105
+  },
+  {
+    kanji: '楽しむ',
+    def: 'enjoy',
+    romaji: '[tanoshimu]',
+    speech_part: 'VERB',
+    id:106
+  },
+  {
+    kanji: '色',
+    def: 'color',
+    romaji: '[iro]',
+    speech_part: 'NOUN',
+    id:107
+  },
+  {
+    kanji: 'みんな',
+    def: 'Everyone',
+    romaji: "[min'na]",
+    speech_part: 'NOUN',
+    id:108
+  },
+  {
+    kanji: '取る',
+    def: 'take, out',
+    romaji: '[toru]',
+    speech_part: '',
+    id:109
+  },
+  {
+    kanji: '勉強',
+    def: 'study',
+    romaji: '[benkyō]',
+    speech_part: 'VERBAL NOUN',
+    id:110
+  },
   // {
   //   kanji: '',
   //   def: '',
