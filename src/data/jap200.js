9fd866bdb2a0e91afc9529afb3917e26935cbda0
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-06-28 13:33:34 -1000

    now include 140 words

--- src/data/jap200.js
+++ src/data/jap200.js
@@ -69,6 +69,216 @@ const JapData = [
     speech_part: 'VERBAL NOUN',
     id:110
   },
+  {
+    kanji: 'できる',
+    def: 'can do, be good at',
+    romaji: '[dekiru]',
+    speech_part: 'VERB',
+    id:111
+  },
+  {
+    kanji: '短い',
+    def: 'short, brief',
+    romaji: '[mijikai]',
+    speech_part: 'ADJ.',
+    id:112
+  },
+  {
+    kanji: '落ちる',
+    def: 'fall, come down',
+    romaji: '[ochiru]',
+    speech_part: 'VERB',
+    id:113
+  },
+  {
+    kanji: '息子',
+    def: 'son',
+    romaji: '[musuko]',
+    speech_part: 'NOUN',
+    id:114
+  },
+  {
+    kanji: '白い',
+    def: 'white, blank',
+    romaji: '[shiroi]',
+    speech_part: 'ADJ.',
+    id:115
+  },
+  {
+    kanji: '飛行機',
+    def: 'airplane',
+    romaji: '[hikōki]',
+    speech_part: 'NOUN',
+    id:116
+  },
+  {
+    kanji: '病気',
+    def: 'illness',
+    romaji: '[byōki]',
+    speech_part: 'NOUN',
+    id:117
+  },
+  {
+    kanji: '冬',
+    def: 'winter',
+    romaji: '[fuyu]',
+    speech_part: 'NOUN',
+    id:118
+  },
+  {
+    kanji: '年',
+    def: 'year, age',
+    romaji: '[toshi]',
+    speech_part: 'NOUN',
+    id:119
+  },
+  {
+    kanji: '重い',
+    def: 'heavy',
+    romaji: '[omoi]',
+    speech_part: 'ADJ.',
+    id:120
+  },
+  {
+    kanji: '胸',
+    def: 'chest, breast',
+    romaji: '[mune]',
+    speech_part: 'NOUN',
+    id:121
+  },
+  {
+    kanji: '払う',
+    def: 'pay (money, respect, attention, etc.)',
+    romaji: '[harau]',
+    speech_part: 'VERB',
+    id:122
+  },
+  {
+    kanji: '軽い',
+    def: 'light (weight, degree, etc.)',
+    romaji: '[karui]',
+    speech_part: 'ADJ.',
+    id:123
+  },
+  {
+    kanji: '見つける ',
+    def: 'find',
+    romaji: '[mitsukeru]',
+    speech_part: 'VERB',
+    id:124
+  },
+  {
+    kanji: '忘れる',
+    def: 'forget, leave behind',
+    romaji: '[wasureru]',
+    speech_part: 'VERB',
+    id:125
+  },
+  {
+    kanji: '酒',
+    def: 'alcohol, rice wine',
+    romaji: '[sake]',
+    speech_part: 'NOUN',
+    id:126
+  },
+  {
+    kanji: 'どちら',
+    def: 'which (polite)',
+    romaji: '[dochira]',
+    speech_part: 'INTERROGATIVE',
+    id:127
+  },
+  {
+    kanji: '姉',
+    def: "(one's own) sister",
+    romaji: '[ane]',
+    speech_part: 'NOUN',
+    id:128
+  },
+  {
+    kanji: '覚える',
+    def: 'memorize, learn',
+    romaji: '[oboeru]',
+    speech_part: 'VERB',
+    id:129
+  },
+  {
+    kanji: '狭い',
+    def: 'narrow, small',
+    romaji: '[semai]',
+    speech_part: 'ADJ.',
+    id:130
+  },
+  {
+    kanji: '赤い',
+    def: 'red',
+    romaji: '[akai]',
+    speech_part: 'ADJ.',
+    id:131
+  },
+  {
+    kanji: '着る',
+    def: 'wear, put on',
+    romaji: '[kiru]',
+    speech_part: 'VERB',
+    id:132
+  },
+  {
+    kanji: '笑う',
+    def: 'laugh',
+    romaji: '[warau]',
+    speech_part: 'VERB',
+    id:133
+  },
+  {
+    kanji: '一番',
+    def: 'best, most',
+    romaji: '[ichiban]',
+    speech_part: 'ADJ.',
+    id:134
+  },
+  {
+    kanji: '授業',
+    def: 'class session, lecture',
+    romaji: '[jugyō]',
+    speech_part: 'VERBIAL NOUN',
+    id:135
+  },
+  {
+    kanji: '週',
+    def: 'week',
+    romaji: '[shū]',
+    speech_part: 'NOUN',
+    id:136
+  },
+  {
+    kanji: '漢字',
+    def: 'chinese character',
+    romaji: '[kanji]',
+    speech_part: 'NOUN',
+    id:137
+  },
+  {
+    kanji: '自転車',
+    def: 'bicycle',
+    romaji: '[jidensha]',
+    speech_part: 'NOUN',
+    id:138
+  },
+  {
+    kanji: '電車',
+    def: 'train',
+    romaji: '[densha]',
+    speech_part: 'NOUN',
+    id:139
+  },
+  {
+    kanji: '探す',
+    def: 'search for, look for',
+    romaji: '[sagasu]',
+    speech_part: 'VERB',
+    id:140
+  },
   // {
   //   kanji: '',
   //   def: '',
