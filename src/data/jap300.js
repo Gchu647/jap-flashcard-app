51bec865067c9d8a8c6c1a8f20e7a4cd64f86b1f
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-07-02 08:57:42 -1000

    now include 230 jap words

--- src/data/jap300.js
+++ src/data/jap300.js
@@ -70,12 +70,159 @@ const JapData = [
     id:210
   },
   {
-    kanji: '',
-    def: '',
-    romaji: '',
-    speech_part: '',
-    id:0
+    kanji: 'お母さん',
+    def: 'mother, mom',
+    romaji: '[okāsan]',
+    speech_part: 'NOUN',
+    id:211
   },
+  {
+    kanji: '大学生',
+    def: 'college student',
+    romaji: '[dagakusei]',
+    speech_part: 'NOUN',
+    id:212
+  },
+  {
+    kanji: '終わり',
+    def: 'end, finish',
+    romaji: '[owari]',
+    speech_part: 'NOUN',
+    id:213
+  },
+  {
+    kanji: '背',
+    def: 'height, stature',
+    romaji: '[se]',
+    speech_part: 'NOUN',
+    id:214
+  },
+  {
+    kanji: '手伝う',
+    def: 'help, assist',
+    romaji: '[tetsudao]',
+    speech_part: 'VERB',
+    id:215
+  },
+  {
+    kanji: '鼻',
+    def: 'nose',
+    romaji: '[hana]',
+    speech_part: 'NOUN',
+    id:216
+  },
+  {
+    kanji: '起きる',
+    def: "occur, happen (2nd def)",
+    romaji: '[okiru]',
+    speech_part: 'VERB',
+    id:217
+  },
+  {
+    kanji: '載せる',
+    def: 'place, put on',
+    romaji: '[noseru]',
+    speech_part: 'VERB',
+    id:218
+  },
+  {
+    kanji: '悲しい',
+    def: 'sad',
+    romaji: '[kanashi]',
+    speech_part: 'ADJ.',
+    id:219
+  },
+  {
+    kanji: 'しゃべる',
+    def: 'chat, talk',
+    romaji: '[shaberu]',
+    speech_part: 'VERB',
+    id:220
+  },
+  {
+    kanji: '近く',
+    def: 'in the near future, before long (2nd def)',
+    romaji: '[chikaku]',
+    speech_part: 'ADV.',
+    id:221
+  },
+  {
+    kanji: '甘い',
+    def: 'sweet',
+    romaji: '[amai]',
+    speech_part: 'ADJ.',
+    id:222
+  },
+  {
+    kanji: 'テーブル',
+    def: 'table',
+    romaji: '[tēburu]',
+    speech_part: 'NOUN',
+    id:223
+  },
+  {
+    kanji: '食べ物 ',
+    def: 'food',
+    romaji: '[tabemono]',
+    speech_part: 'NOUN',
+    id:224
+  },
+  {
+    kanji: '始まる',
+    def: 'begin',
+    romaji: '[hajimaru]',
+    speech_part: 'VERB',
+    id:225
+  },
+  {
+    kanji: 'ゲーム',
+    def: 'game',
+    romaji: '[gēmu]',
+    speech_part: 'NOUN',
+    id:226
+  },
+  {
+    kanji: '十',
+    def: 'ten',
+    romaji: '[jū]',
+    speech_part: 'NOUN',
+    id:227
+  },
+  {
+    kanji: '天気',
+    def: 'weather',
+    romaji: '[tenki]',
+    speech_part: 'NOUN',
+    id:228
+  },
+  {
+    kanji: '暑い',
+    def: 'hot (of weather)',
+    romaji: '[atsui]',
+    speech_part: 'ADJ.',
+    id:229
+  },
+  {
+    kanji: '太い',
+    def: 'thick, fat',
+    romaji: '[futoi]',
+    speech_part: 'ADJ.',
+    id:230
+  },
+  // {
+  //   kanji: '晩',
+  //   def: 'evening, night',
+  //   romaji: '[ban]',
+  //   speech_part: '',
+  //   id:0
+  // },
+  // {
+  //   kanji: '',
+  //   def: '',
+  //   romaji: '',
+  //   speech_part: '',
+  //   id:0
+  // },
 ];
 
 export default JapData;
