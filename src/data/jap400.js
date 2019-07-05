d5b29b8669d27f633af66e91c6dd1256d2fbfa6d
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-07-04 21:04:33 -1000

    now include 340 words

--- src/data/jap400.js
+++ src/data/jap400.js
@@ -79,7 +79,7 @@ const JapData = [
   {
     kanji: '酸っぱい',
     def: 'sour',
-    romaji: '[suppai',
+    romaji: '[suppai]',
     speech_part: 'ADJ.',
     id:312
   },
@@ -99,7 +99,7 @@ const JapData = [
   },
   {
     kanji: '三つ',
-    def: 'three',
+    def: 'three (things)',
     romaji: '[mitsu]',
     speech_part: 'NOUN',
     id:315
@@ -139,13 +139,153 @@ const JapData = [
     speech_part: 'NOUN',
     id:320
   },
+  {
+    kanji: '二十日',
+    def: '20 days, 20th of the month',
+    romaji: '[hatsuka]',
+    speech_part: 'NOUN',
+    id:321
+  },
+  {
+    kanji: '左',
+    def: 'left',
+    romaji: '[hidari]',
+    speech_part: 'NOUN',
+    id:322
+  },
+  {
+    kanji: '二日',
+    def: 'two days, 2nd day of the month',
+    romaji: '[futsuka]',
+    speech_part: 'NOUN',
+    id:323
+  },
+  {
+    kanji: '四つ',
+    def: 'four (things)',
+    romaji: '[yottsu]',
+    speech_part: 'NOUN',
+    id:324
+  },
+  {
+    kanji: '暖かい',
+    def: 'warm',
+    romaji: '[atatakai]',
+    speech_part: 'ADJ',
+    id:325
+  },
+  {
+    kanji: 'ある',
+    def: 'exist, there is',
+    romaji: '[aru]',
+    speech_part: 'VERB',
+    id:326
+  },
+  {
+    kanji: 'いい',
+    def: 'good (informal/spoken)',
+    romaji: '[ii]',
+    speech_part: 'ADJ.',
+    id:327
+  },
+  {
+    kanji: '五つ ',
+    def: 'five (things)',
+    romaji: '[itsutsu]',
+    speech_part: 'NOUN',
+    id:328
+  },
+  {
+    kanji: '妹',
+    def: 'younger sister',
+    romaji: '[imōto]',
+    speech_part: 'NOUN',
+    id:329
+  },
+  {
+    kanji: '上',
+    def: 'up, above',
+    romaji: '[ue]',
+    speech_part: 'VERB',
+    id:330
+  },
+  {
+    kanji: '動く',
+    def: 'move',
+    romaji: '[ugoku]',
+    speech_part: 'VERB',
+    id:331
+  },
+  {
+    kanji: '駅',
+    def: 'train station',
+    romaji: '[eki]',
+    speech_part: 'NOUN',
+    id:332
+  },
+  {
+    kanji: '美味しい',
+    def: 'tasty',
+    romaji: '[oishi]',
+    speech_part: 'ADJ.',
+    id:333
+  },
+  {
+    kanji: '大きい',
+    def: 'big',
+    romaji: '[Ōkī]',
+    speech_part: 'ADJ.',
+    id:334
+  },
+  {
+    kanji: '弟',
+    def: 'younger brother',
+    romaji: '[otōto]',
+    speech_part: 'NOUN',
+    id:335
+  },
+  {
+    kanji: '男',
+    def: 'man, male',
+    romaji: '[otoko]',
+    speech_part: 'NOUN',
+    id:336
+  },
+  {
+    kanji: 'お願い',
+    def: 'favor',
+    romaji: '[onegai]',
+    speech_part: 'VERBAL NOUN',
+    id:337
+  },
+  {
+    kanji: '女',
+    def: 'woman',
+    romaji: '[ona]',
+    speech_part: 'NOUN',
+    id:338
+  },
+  {
+    kanji: '金',
+    def: 'money',
+    romaji: '[kane]',
+    speech_part: 'NOUN',
+    id:339
+  },
+  {
+    kanji: '彼',
+    def: 'he, boyfriend',
+    romaji: '[kare]',
+    speech_part: 'PRONOUN',
+    id:340
+  },
   // {
   //   kanji: '',
   //   def: '',
   //   romaji: '',
   //   speech_part: '',
   //   id:0
-  // },
+  // }
 ];
 
 export default JapData;
