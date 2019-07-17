91f6d6fd63aab10031ea1edb774d6406e3715f70
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-07-16 23:37:50 -1000

    now include 620 jap words

---
+++ src/data/jap700.js
@@ -0,0 +1,151 @@
+const JapData = [
+  {
+    kanji: '集まる',
+    def: 'be collected, be gathered',
+    romaji: '[atsumaru]',
+    speech_part: 'VERB',
+    id:601
+  },
+  {
+    kanji: '安全',
+    def: 'safety, security',
+    romaji: '[anzen]',
+    speech_part: 'ADJ. NOUN',
+    id:602
+  },
+  {
+    kanji: '海',
+    def: 'sea, ocean',
+    romaji: '[umi]',
+    speech_part: 'NOUN',
+    id:603
+  },
+  {
+    kanji: '運動',
+    def: 'movement, exercise',
+    romaji: '[undō]',
+    speech_part: 'VERBAL NOUN',
+    id:604
+  },
+  {
+    kanji: '映画',
+    def: 'movie',
+    romaji: '[eiga]',
+    speech_part: 'NOUN',
+    id:605
+  },
+  {
+    kanji: '英語',
+    def: 'English',
+    romaji: '[eigo]',
+    speech_part: 'NOUN',
+    id:606
+  },
+  {
+    kanji: '遅れる',
+    def: 'be late',
+    romaji: '[okureru]',
+    speech_part: 'VERB',
+    id:607
+  },
+  {
+    kanji: '音',
+    def: 'sound, noise',
+    romaji: '[oto]',
+    speech_part: 'NOUN',
+    id:608
+  },
+  {
+    kanji: '驚く',
+    def: 'be surprised, be startled',
+    romaji: '[odoroku]',
+    speech_part: 'VERB',
+    id:609
+  },
+  {
+    kanji: '海外',
+    def: 'overseas, abroad',
+    romaji: '[kaigai]',
+    speech_part: 'NOUN',
+    id:610
+  },
+  {
+    kanji: '外国人 ',
+    def: 'foreigner',
+    romaji: '[gaikoku jin]',
+    speech_part: 'NOUN',
+    id:611
+  },
+  {
+    kanji: '会議',
+    def: 'meeting, conference',
+    romaji: '[kaigi]',
+    speech_part: 'VERBAL NOUN',
+    id:612
+  },
+  {
+    kanji: '確認',
+    def: 'confirmation',
+    romaji: '[kakunin]',
+    speech_part: 'VERBAL NOUN',
+    id:613
+  },
+  {
+    kanji: '課題',
+    def: 'task, assignment',
+    romaji: '[kadai]',
+    speech_part: 'NOUN',
+    id:614
+  },
+  {
+    kanji: '家庭',
+    def: 'home, family',
+    romaji: '[katei]',
+    speech_part: 'NOUN',
+    id:615
+  },
+  {
+    kanji: '彼ら',
+    def: 'they',
+    romaji: '[karera]',
+    speech_part: 'PRONOUN',
+    id:616
+  },
+  {
+    kanji: '簡単',
+    def: 'simple, easy',
+    romaji: '[kantan]',
+    speech_part: 'ADJ. NOUN',
+    id:617
+  },
+  {
+    kanji: '機械',
+    def: 'machine',
+    romaji: '[kikai]',
+    speech_part: 'NOUN',
+    id:618
+  },
+  {
+    kanji: '期間',
+    def: 'term, period',
+    romaji: '[kikan]',
+    speech_part: 'NOUN',
+    id:619
+  },
+  {
+    kanji: '危険',
+    def: 'danger, dangerous',
+    romaji: '[kiken]',
+    speech_part: 'ADJ. NOUN',
+    id:620
+  },
+  // {
+  //   kanji: '',
+  //   def: '',
+  //   romaji: '',
+  //   speech_part: '',
+  //   id:61
+  // },
+];
+
+export default JapData;
