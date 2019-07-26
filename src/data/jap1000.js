9ba7476f82f13dbca0f79b3c970691e4e0a6eccf
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-07-26 00:27:00 -1000

    now include 920 jap words

---
+++ src/data/jap1000.js
@@ -0,0 +1,151 @@
+const JapData = [
+  {
+    kanji: '隣',
+    def: 'next to, next door',
+    romaji: '[tonari]',
+    speech_part: 'NOUN',
+    id:901
+  },
+  {
+    kanji: 'トラック',
+    def: 'truck',
+    romaji: '[torakku]',
+    speech_part: 'NOUN',
+    id:902
+  },
+  {
+    kanji: '撮る',
+    def: 'take (a photograph), film',
+    romaji: '[toru]',
+    speech_part: 'VERB',
+    id:903
+  },
+  {
+    kanji: 'どんどん',
+    def: 'knock, bang',
+    romaji: '[dondon]',
+    speech_part: 'ADV.',
+    id:904
+  },
+  {
+    kanji: '長さ',
+    def: 'length',
+    romaji: '[naga-sa]',
+    speech_part: 'NOUN',
+    id:905
+  },
+  {
+    kanji: '流れる',
+    def: 'flow, run',
+    romaji: '[naga-reru]',
+    speech_part: 'VERB',
+    id:906
+  },
+  {
+    kanji: '亡くなる',
+    def: 'die, pass away',
+    romaji: '[naku-naru]',
+    speech_part: 'VERB',
+    id:907
+  },
+  {
+    kanji: '何か',
+    def: 'something, some',
+    romaji: '[nanika]',
+    speech_part: 'PRONOUN',
+    id:908
+  },
+  {
+    kanji: '涙',
+    def: 'tear',
+    romaji: '[namita]',
+    speech_part: 'NOUN',
+    id:909
+  },
+  {
+    kanji: '並べる',
+    def: 'line up, arrange',
+    romaji: '[nara-beru]',
+    speech_part: 'VERB',
+    id:910
+  },
+  {
+    kanji: '慣れる',
+    def: 'grow accustomed to, get used to',
+    romaji: '[nareru]',
+    speech_part: 'VERB',
+    id:911
+  },
+  {
+    kanji: '逃げる',
+    def: 'escape, run away',
+    romaji: '[nigeru]',
+    speech_part: 'VERB',
+    id:912
+  },
+  {
+    kanji: '入院',
+    def: 'hospitalization',
+    romaji: '[nyūin]',
+    speech_part: 'VERBAL NOUN',
+    id:913
+  },
+  {
+    kanji: '入学',
+    def: 'enter a school, matriculate',
+    romaji: '[nyūgaku]',
+    speech_part: 'VERBAL NOUN',
+    id:914
+  },
+  {
+    kanji: 'ニュース',
+    def: 'news',
+    romaji: '[nyūsu]',
+    speech_part: 'NOUN',
+    id:915
+  },
+  {
+    kanji: '値段',
+    def: 'price',
+    romaji: '[nedan]',
+    speech_part: 'NOUN',
+    id:916
+  },
+  {
+    kanji: '熱',
+    def: 'heat, fever',
+    romaji: '[netsu]',
+    speech_part: 'NOUN',
+    id:917
+  },
+  {
+    kanji: '伸びる',
+    def: 'stretch, grow',
+    romaji: '[nobiru]',
+    speech_part: 'VERB',
+    id:918
+  },
+  {
+    kanji: 'パーティー',
+    def: 'party',
+    romaji: '[pātī]',
+    speech_part: 'NOUN',
+    id:919
+  },
+  {
+    kanji: '早く',
+    def: 'early, soon',
+    romaji: '[hayaku]',
+    speech_part: 'ADV.',
+    id:920
+  },
+  // {
+  //   kanji: '',
+  //   def: '',
+  //   romaji: '',
+  //   speech_part: '',
+  //   id:92
+  // },
+];
+
+export default JapData;
