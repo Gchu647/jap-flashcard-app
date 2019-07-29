78d252c118896eba6a0062ddb45daf006cc705cd
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-07-28 22:35:53 -1000

    now include 1020 jap words

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import JapData from '../../data/jap1000';
+import JapData from '../../data/jap1100';
 import FlashcardTop from '../FlascardTop/FlashcardTop';
 import FlashcardBottom from '../FlashcardBottom/FlashcardBottom';
 
@@ -15,8 +15,8 @@ class App extends Component {
       displayTop: false, // FlashcardTop
       displayBottom: false, // FlashcardBottom
       learnCount: 0, // when to stop pickLearnWord method
-      startId: 981, // start of our vocab list
-      endId: 1000, // end of our vocab list
+      startId: 1001, // start of our vocab list
+      endId: 1020, // end of our vocab list
     }
 
     this.quiz = this.quiz.bind(this);
@@ -43,7 +43,7 @@ class App extends Component {
   componentDidMount() { // create a clone of japData and store it to our state
     this.setState((prevState) => { // proper way to increment
       // this needs to be changed when working with jap 200 and above
-      return {vocabList: JapData.slice(prevState.startId-901, prevState.endId-900)}
+      return {vocabList: JapData.slice(prevState.startId-1001, prevState.endId-1000)}
     });
   }
 
---
+++ src/data/jap1100.js
@@ -0,0 +1,151 @@
+const JapData = [
+  {
+    kanji: '浅い',
+    def: 'shallow, superficial',
+    romaji: '[asai]',
+    speech_part: 'ADJ.',
+    id:1001
+  },
+  {
+    kanji: '預ける',
+    def: 'deposit/leave (something) with (someone)',
+    romaji: '[azukeru]',
+    speech_part: 'VERB',
+    id:1002
+  },
+  {
+    kanji: 'アルバイト',
+    def: 'part-time job',
+    romaji: '[arubaito]',
+    speech_part: 'VERBAL NOUN',
+    id:1003
+  },
+  {
+    kanji: '安心',
+    def: 'peace of mind, relief',
+    romaji: '[ashin]',
+    speech_part: 'VERBAL NOUN',
+    id:1004
+  },
+  {
+    kanji: '案内',
+    def: 'guide, guidance',
+    romaji: '[ainan]',
+    speech_part: 'VERBAL NOUN',
+    id:1005
+  },
+  {
+    kanji: '言い方',
+    def: 'way of speaking, expression',
+    romaji: '[iikata]',
+    speech_part: 'NOUN',
+    id:1006
+  },
+  {
+    kanji: '池',
+    def: 'pond',
+    romaji: '[ike]',
+    speech_part: 'NOUN',
+    id:1007
+  },
+  {
+    kanji: '石',
+    def: 'stone, small rock',
+    romaji: '[ishi]',
+    speech_part: 'NOUN',
+    id:1008
+  },
+  {
+    kanji: '医者',
+    def: 'doctor',
+    romaji: '[isha]',
+    speech_part: 'NOUN',
+    id:1009
+  },
+  {
+    kanji: '牛',
+    def: 'cattle, cow',
+    romaji: '[ushi]',
+    speech_part: 'NOUN',
+    id:1010
+  },
+  {
+    kanji: '内側',
+    def: 'inside, interior',
+    romaji: '[uchigawa]',
+    speech_part: 'NOUN',
+    id:1011
+  },
+  {
+    kanji: '生む',
+    def: 'give birth, produce',
+    romaji: '[umu]',
+    speech_part: 'VERB',
+    id:1012
+  },
+  {
+    kanji: '運転手',
+    def: 'driver',
+    romaji: '[untenshu]',
+    speech_part: 'NOUN',
+    id:1013
+  },
+  {
+    kanji: '絵',
+    def: 'picture, painting',
+    romaji: '[e]',
+    speech_part: 'NOUN',
+    id:1014
+  },
+  {
+    kanji: '往復',
+    def: 'round trip',
+    romaji: '[Ōfuku]',
+    speech_part: 'NOUN',
+    id:1015
+  },
+  {
+    kanji: '大勢',
+    def: 'large numbers, a lot of (people)',
+    romaji: '[taisei]',
+    speech_part: 'NOUN',
+    id:1016
+  },
+  {
+    kanji: 'オートバイ',
+    def: 'motorbike, motorcycle',
+    romaji: '[Ōtobai]',
+    speech_part: 'NOUN',
+    id:1017
+  },
+  {
+    kanji: 'おかしい',
+    def: 'strange, peculiar',
+    romaji: '[okashī]',
+    speech_part: 'ADJ.',
+    id:1018
+  },
+  {
+    kanji: '奥',
+    def: 'inner part, the back',
+    romaji: '[oku]',
+    speech_part: 'NOUN',
+    id:1019
+  },
+  {
+    kanji: '奥さん',
+    def: 'married woman',
+    romaji: '[okusan]',
+    speech_part: 'NOUN',
+    id:1020
+  },
+  // {
+  //   kanji: '',
+  //   def: '',
+  //   romaji: '',
+  //   speech_part: '',
+  //   id:102
+  // },
+];
+
+export default JapData;
