660bfcddd317fd813d85cdac1bac1e12c7170093
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-08-01 09:26:54 -1000

    now include 1140 jap words

--- src/data/jap1200.js
+++ src/data/jap1200.js
@@ -139,12 +139,152 @@ const JapData = [
     speech_part: 'NOUN',
     id:1120
   },
+  {
+    kanji: 'ノート',
+    def: 'notebook (loan word)',
+    romaji: '[nōto]',
+    speech_part: 'NOUN',
+    id:1121
+  },
+  {
+    kanji: '葉',
+    def: 'leaf',
+    romaji: '[ha]',
+    speech_part: 'NOUN',
+    id:1122
+  },
+  {
+    kanji: '博物館',
+    def: 'museum',
+    romaji: '[haku-butsu-kan]',
+    speech_part: 'NOUN',
+    id:1123
+  },
+  {
+    kanji: '箱',
+    def: 'box, case',
+    romaji: '[hako]',
+    speech_part: 'NOUN',
+    id:1124
+  },
+  {
+    kanji: 'パン',
+    def: 'bread',
+    romaji: '[pan]',
+    speech_part: 'NOUN',
+    id:1125
+  },
+  {
+    kanji: '番号',
+    def: 'assigned (phone) number',
+    romaji: '[bangō]',
+    speech_part: 'NOUN',
+    id:1126
+  },
+  {
+    kanji: 'ピアノ',
+    def: 'piano',
+    romaji: '[piano]',
+    speech_part: 'NOUN',
+    id:1127
+  },
+  {
+    kanji: '光る',
+    def: 'shine, sparkle',
+    romaji: '[hikaru]',
+    speech_part: 'VERB',
+    id:1128
+  },
+  {
+    kanji: '久しぶり',
+    def: 'after a long time',
+    romaji: '[hisashi-buri]',
+    speech_part: 'NOUN',
+    id:1129
+  },
+  {
+    kanji: '左手',
+    def: 'left hand',
+    romaji: '[hidaride]',
+    speech_part: 'NOUN',
+    id:1130
+  },
+  {
+    kanji: 'びっくりする',
+    def: 'be surprised, bt startled (casual)',
+    romaji: '[bikkuri-suru]',
+    speech_part: 'VERB',
+    id:1131
+  },
+  {
+    kanji: 'ひどい',
+    def: 'cruel, awful',
+    romaji: '[hidoi]',
+    speech_part: 'ADJ.',
+    id:1132
+  },
+  {
+    kanji: '拾う',
+    def: 'pick up',
+    romaji: '[hirou]',
+    speech_part: 'VERB',
+    id:1133
+  },
+  {
+    kanji: 'プール',
+    def: '(swim) pool',
+    romaji: '[pūru]',
+    speech_part: 'NOUN',
+    id:1134
+  },
+  {
+    kanji: '不幸',
+    def: 'unhappiness, misfortune',
+    romaji: '[fukō]',
+    speech_part: 'ADJ. NOUN',
+    id:1135
+  },
+  {
+    kanji: 'ぶつかる',
+    def: 'hit, bump into',
+    romaji: '[butsu-karu]',
+    speech_part: 'VERB',
+    id:1136
+  },
+  {
+    kanji: '踏む',
+    def: 'step on, tread on',
+    romaji: '[fumu]',
+    speech_part: 'VERB',
+    id:1137
+  },
+  {
+    kanji: 'プラスチック',
+    def: 'plastic',
+    romaji: '[purasu-chikku]',
+    speech_part: 'NOUN',
+    id:1138
+  },
+  {
+    kanji: 'ベッド',
+    def: 'bed',
+    romaji: '[beddo]',
+    speech_part: 'NOUN',
+    id:1139
+  },
+  {
+    kanji: '別々',
+    def: 'separately',
+    romaji: '[betsu-betsu]',
+    speech_part: 'ADJ. NOUN',
+    id:1140
+  },
   // {
   //   kanji: '',
   //   def: '',
   //   romaji: '',
   //   speech_part: '',
-  //   id:112
+  //   id:114
   // },
 ];
 
