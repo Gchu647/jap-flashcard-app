d9cd98f9118c3de1932e71a1d58781d712a1d16c
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-08-04 12:53:59 -1000

    now include 1220 jap words

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import JapData from '../../data/jap1200';
+import JapData from '../../data/jap1300';
 import FlashcardTop from '../FlascardTop/FlashcardTop';
 import FlashcardBottom from '../FlashcardBottom/FlashcardBottom';
 
@@ -15,8 +15,8 @@ class App extends Component {
       displayTop: false, // FlashcardTop
       displayBottom: false, // FlashcardBottom
       learnCount: 0, // when to stop pickLearnWord method
-      startId: 1181, // start of our vocab list
-      endId: 1200, // end of our vocab list
+      startId: 1211, // start of our vocab list
+      endId: 1220, // end of our vocab list
     }
 
     this.quiz = this.quiz.bind(this);
@@ -43,7 +43,7 @@ class App extends Component {
   componentDidMount() { // create a clone of japData and store it to our state
     this.setState((prevState) => { // proper way to increment
       // this needs to be changed when working with jap 200 and above
-      return {vocabList: JapData.slice(prevState.startId-1101, prevState.endId-1100)}
+      return {vocabList: JapData.slice(prevState.startId-1201, prevState.endId-1200)}
     });
   }
 
---
+++ src/data/jap1300.js
@@ -0,0 +1,151 @@
+const JapData = [
+  {
+    kanji: '果物',
+    def: 'fruit',
+    romaji: '[kudamono]',
+    speech_part: 'NOUN',
+    id:1201
+  },
+  {
+    kanji: '間違い',
+    def: 'mistake, error',
+    romaji: '[machigai]',
+    speech_part: 'NOUN',
+    id:1202
+  },
+  {
+    kanji: '映画館',
+    def: 'movie theater',
+    romaji: '[eiga-kan]',
+    speech_part: 'NOUN',
+    id:1203
+  },
+  {
+    kanji: '怖い',
+    def: 'scary, be afraid',
+    romaji: '[kowai]',
+    speech_part: 'ADJ.',
+    id:1204
+  },
+  {
+    kanji: '鏡',
+    def: 'mirror',
+    romaji: '[kagami]',
+    speech_part: 'NOUN',
+    id:1205
+  },
+  {
+    kanji: '草',
+    def: 'grass, weed',
+    romaji: '[kusa]',
+    speech_part: 'NOUN',
+    id:1206
+  },
+  {
+    kanji: '敬語',
+    def: 'polite speech',
+    romaji: '[keigo]',
+    speech_part: 'NOUN',
+    id:1207
+  },
+  {
+    kanji: '欠席',
+    def: 'absence (from school, event, etc.)',
+    romaji: '[kesseki]',
+    speech_part: 'VERBAL NOUN',
+    id:1208
+  },
+  {
+    kanji: 'スイッチ',
+    def: 'switch',
+    romaji: '[suitchi]',
+    speech_part: 'NOUN',
+    id:1209
+  },
+  {
+    kanji: '掃除',
+    def: 'cleaning',
+    romaji: '[sōji]',
+    speech_part: 'VERBAL NOUN',
+    id:1210
+  },
+  {
+    kanji: '畳',
+    def: 'straw mat',
+    romaji: '[tatami]',
+    speech_part: 'NOUN',
+    id:1211
+  },
+  {
+    kanji: '速さ',
+    def: 'speed',
+    romaji: '[hayasa]',
+    speech_part: 'NOUN',
+    id:1212
+  },
+  {
+    kanji: '林',
+    def: 'woods, forest',
+    romaji: '[hayashi]',
+    speech_part: 'NOUN',
+    id:1213
+  },
+  {
+    kanji: 'プレゼント',
+    def: 'present (loan word)',
+    romaji: '[purezento]',
+    speech_part: 'NOUN',
+    id:1214
+  },
+  {
+    kanji: '弁当',
+    def: 'boxed lunch',
+    romaji: '[bentō]',
+    speech_part: 'NOUN',
+    id:1215
+  },
+  {
+    kanji: 'いつでも',
+    def: 'at all times, any time',
+    romaji: '[itsu-demo]',
+    speech_part: 'ADV.',
+    id:1216
+  },
+  {
+    kanji: '寂しい',
+    def: 'lonely, desolate',
+    romaji: '[sabishī]',
+    speech_part: 'ADJ.',
+    id:1217
+  },
+  {
+    kanji: '少しも',
+    def: '(not) in the least',
+    romaji: '[sukoshimo]',
+    speech_part: 'ADV.',
+    id:1218
+  },
+  {
+    kanji: '尊敬',
+    def: 'respect, esteem',
+    romaji: '[sonkei]',
+    speech_part: 'VERBAL NOUN',
+    id:1219
+  },
+  {
+    kanji: 'テニス',
+    def: 'tennis',
+    romaji: '[tenisu]',
+    speech_part: 'NOUN',
+    id:1220
+  },
+  // {
+  //   kanji: '',
+  //   def: '',
+  //   romaji: '',
+  //   speech_part: '',
+  //   id:122
+  // },
+];
+
+export default JapData;
