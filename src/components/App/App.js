b793f9ef59f1761048de981f77d0612fc73c4d5c
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-08-16 08:56:51 -1000

    now include 1640 jap words

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,8 +15,8 @@ class App extends Component {
       displayTop: false, // FlashcardTop
       displayBottom: false, // FlashcardBottom
       learnCount: 0, // when to stop pickLearnWord method
-      startId: 1601, // start of our vocab list
-      endId: 1620, // end of our vocab list
+      startId: 1621, // start of our vocab list
+      endId: 1640, // end of our vocab list
     }
 
     this.quiz = this.quiz.bind(this);
--- src/data/jap1700.js
+++ src/data/jap1700.js
@@ -16,7 +16,7 @@ const JapData = [
   {
     kanji: 'バスケットボール',
     def: 'basketball',
-    romaji: '[basu-ketto-bōru',
+    romaji: '[basu-ketto-bōru]',
     speech_part: 'NOUN',
     id:1603
   },
@@ -51,7 +51,7 @@ const JapData = [
   {
     kanji: '降ろす',
     def: 'unload, set down',
-    romaji: '[orosu]',
+    romaji: '[orosu] (2)',
     speech_part: 'VERB',
     id:1608
   },
@@ -139,12 +139,152 @@ const JapData = [
     speech_part: 'VERBAL NOUN',
     id:1620
   },
+  {
+    kanji: '灰皿',
+    def: 'ash tray',
+    romaji: '[aizara]',
+    speech_part: 'NOUN',
+    id:1621
+  },
+  {
+    kanji: 'ハム',
+    def: 'ham',
+    romaji: '[hamu]',
+    speech_part: 'NOUN',
+    id:1622
+  },
+  {
+    kanji: 'ボーイフレンド',
+    def: 'boyfriend',
+    romaji: '[bōi-furendo]',
+    speech_part: 'NOUN',
+    id:1623
+  },
+  {
+    kanji: '床',
+    def: 'floor',
+    romaji: '[yuka]',
+    speech_part: 'NOUN',
+    id:1624
+  },
+  {
+    kanji: 'ローマ字',
+    def: 'roman alphabet',
+    romaji: '[rōmaji]',
+    speech_part: 'NOUN',
+    id:1625
+  },
+  {
+    kanji: 'いちご',
+    def: 'strawberry',
+    romaji: '[ichigo]',
+    speech_part: 'NOUN',
+    id:1626
+  },
+  {
+    kanji: 'カレーライス',
+    def: 'curry rice',
+    romaji: '[karē-raisu]',
+    speech_part: 'NOUN',
+    id:1627
+  },
+  {
+    kanji: 'キャッシュカード',
+    def: 'cash card',
+    romaji: '[kyasshu-kādo]',
+    speech_part: 'NOUN',
+    id:1628
+  },
+  {
+    kanji: '停留所',
+    def: '(bus) stop',
+    romaji: '[teiryū-jo]',
+    speech_part: 'NOUN',
+    id:1629
+  },
+  {
+    kanji: '床屋',
+    def: 'barber, barbershop',
+    romaji: '[tokoya]',
+    speech_part: 'NOUN',
+    id:1630
+  },
+  {
+    kanji: '歯磨き',
+    def: 'brushing ones teeth',
+    romaji: '[hami-gaki]',
+    speech_part: 'NOUN',
+    id:1631
+  },
+  {
+    kanji: '早起き',
+    def: 'getting up early',
+    romaji: '[haya-oki]',
+    speech_part: 'VERBAL NOUN',
+    id:1632
+  },
+  {
+    kanji: '夕飯',
+    def: 'evening meal, dinner (casual)',
+    romaji: '[yūhan]',
+    speech_part: 'NOUN',
+    id:1633
+  },
+  {
+    kanji: '酔っ払い',
+    def: 'drunkard, drunk',
+    romaji: '[yoppa-rai]',
+    speech_part: 'NOUN',
+    id:1634
+  },
+  {
+    kanji: '例',
+    def: 'example',
+    romaji: '[rei]',
+    speech_part: 'NOUN',
+    id:1635
+  },
+  {
+    kanji: 'イヤリング',
+    def: 'earrings',
+    romaji: '[iya-ringu]',
+    speech_part: 'NOUN',
+    id:1636
+  },
+  {
+    kanji: 'お辞儀 ',
+    def: 'bow',
+    romaji: '[ojigi]',
+    speech_part: 'VERBAL NOUN',
+    id:1637
+  },
+  {
+    kanji: '横断歩道',
+    def: 'crosswalk',
+    romaji: '[Ōdan-hodō]',
+    speech_part: 'NOUN',
+    id:1638
+  },
+  {
+    kanji: 'お宅',
+    def: '(some else) home',
+    romaji: '[otaku]',
+    speech_part: 'NOUN',
+    id:1639
+  },
+  {
+    kanji: 'ガールフレンド',
+    def: 'girlfriend',
+    romaji: '[gāru-furendo]',
+    speech_part: 'NOUN',
+    id:1640
+  },
   // {
   //   kanji: '',
   //   def: '',
   //   romaji: '',
   //   speech_part: '',
-  //   id:162
+  //   id:164
   // },
 ];
 
