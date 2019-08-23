7b840ee461e6bd5234359deb23c968adac28ec01
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-08-23 00:07:07 -1000

    now include 1840 jap words

--- src/data/jap1900.js
+++ src/data/jap1900.js
@@ -21,7 +21,7 @@ const JapData = [
     id:1803
   },
   {
-    kanji: '上がる',
+    kanji: '上がる (2)',
     def: 'go up, increase (in quality or quantity)',
     romaji: '[agaru]',
     speech_part: 'VERB',
@@ -50,7 +50,7 @@ const JapData = [
   },
   {
     kanji: '景色',
-    def: 'natural, scenery or landscape',
+    def: 'scenery or landscape',
     romaji: '[keshiki]',
     speech_part: 'NOUN',
     id:1808
@@ -120,7 +120,7 @@ const JapData = [
   },
   {
     kanji: '血',
-    def: 'blood (colloquial)',
+    def: 'blood (casual)',
     romaji: '[chi]',
     speech_part: 'NOUN',
     id:1818
@@ -139,12 +139,152 @@ const JapData = [
     speech_part: 'NOUN',
     id:1820
   },
+  {
+    kanji: '寺',
+    def: 'temple',
+    romaji: '[dera]',
+    speech_part: 'NOUN',
+    id:1821
+  },
+  {
+    kanji: 'とんとん',
+    def: 'tapping, patting',
+    romaji: '[ton-ton]',
+    speech_part: 'ADV.',
+    id:1822
+  },
+  {
+    kanji: '長袖',
+    def: 'long sleeves',
+    romaji: '[naga-sode]',
+    speech_part: 'NOUN',
+    id:1823
+  },
+  {
+    kanji: '鶏',
+    def: 'rooster, hen',
+    romaji: '[niwa-tori]',
+    speech_part: 'NOUN',
+    id:1824
+  },
+  {
+    kanji: '濡れる',
+    def: 'get wet',
+    romaji: '[nureru]',
+    speech_part: 'VERB',
+    id:1825
+  },
+  {
+    kanji: 'パーセント',
+    def: 'percent',
+    romaji: '[pāsento]',
+    speech_part: 'NOUN',
+    id:1826
+  },
+  {
+    kanji: '灰',
+    def: 'ashes',
+    romaji: '[hai]',
+    speech_part: 'NOUN',
+    id:1827
+  },
+  {
+    kanji: '灰色',
+    def: 'gray, ash color',
+    romaji: '[haiiro]',
+    speech_part: 'NOUN',
+    id:1828
+  },
+  {
+    kanji: '拍手',
+    def: 'applause, clapping',
+    romaji: '[haku-sho]',
+    speech_part: 'VERBAL',
+    id:1829
+  },
+  {
+    kanji: '貼る',
+    def: 'stick, paste',
+    romaji: '[haru] (2)',
+    speech_part: 'VERB',
+    id:1830
+  },
+  {
+    kanji: '半袖',
+    def: 'short sleeves',
+    romaji: '[han-sode]',
+    speech_part: 'NOUN',
+    id:1831
+  },
+  {
+    kanji: '弾く',
+    def: 'play (instruments)',
+    romaji: '[hiku] (2)',
+    speech_part: 'VERB',
+    id:1832
+  },
+  {
+    kanji: '人々',
+    def: 'people',
+    romaji: '[hito-bito]',
+    speech_part: 'NOUN',
+    id:1833
+  },
+  {
+    kanji: 'ひも',
+    def: 'laces, string',
+    romaji: '[himo]',
+    speech_part: 'NOUN',
+    id:1834
+  },
+  {
+    kanji: '昼過ぎ',
+    def: 'early afternoon',
+    romaji: '[hiru-sugi]',
+    speech_part: 'NOUN',
+    id:1835
+  },
+  {
+    kanji: '船',
+    def: 'ship, vessel',
+    romaji: '[fune]',
+    speech_part: 'NOUN',
+    id:1836
+  },
+  {
+    kanji: '星',
+    def: 'star, planet',
+    romaji: '[hoshi] (2)',
+    speech_part: 'NOUN',
+    id:1837
+  },
+  {
+    kanji: 'まく',
+    def: 'scatter, sprinkle',
+    romaji: '[maku] (2)',
+    speech_part: 'VERB',
+    id:1838
+  },
+  {
+    kanji: '港',
+    def: 'port, harbor',
+    romaji: '[minato]',
+    speech_part: 'NOUN',
+    id:1839
+  },
+  {
+    kanji: '紫',
+    def: 'purple',
+    romaji: '[mura-sagi]',
+    speech_part: 'NOUN',
+    id:1840
+  },
   // {
   //   kanji: '',
   //   def: '',
   //   romaji: '',
   //   speech_part: '',
-  //   id:182
+  //   id:184
   // },
 ];
 
